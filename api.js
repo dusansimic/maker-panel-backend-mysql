82203ddba5dfae187c9b6608036247779f66d3da
Nenad Vuletic <vuletic.nenad@jjzmaj.edu.rs>
2019-01-30 20:51:49 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const restRotuer = require('./rest-router');
+const restRouter = require('./rest-router');
 const integrationsRouter = require('./integrations-router');
 
 const api = express.Router(); // eslint-disable-line new-cap
 
-api.use('/rest', restRotuer)
+api.use('/rest', restRouter)
 api.use('/integrations', integrationsRouter);
 
 module.exports = api;
